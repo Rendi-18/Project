143e974aa9a71588dba5a910db0a97291b9c43f6
rendi-18 <rendidwipratama99@gmail.com>
2021-11-04 22:25:03 +0700

    Replace Laravel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Role_User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,43 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Role::create([
+            'name' => 'Anna',
+        ]);
+
+        Role::create([
+            'name' => 'Reiner',
+        ]);
+
+        Role::create([
+            'name' => 'Eren',
+        ]);
+
+        User::create([
+            'name' => 'Action',
+        ]);
+
+        User::create([
+            'name' => 'Romance',
+        ]);
+
+        User::create([
+            'name' => 'Fantasy',
+        ]);
+
+        Role_User::create([
+            'user_id' => 1,
+            'role_id' => 2,
+        ]);
+
+        Role_User::create([
+            'user_id' => 1,
+            'role_id' => 3,
+        ]);
+
+        Role_User::create([
+            'user_id' => 3,
+            'role_id' => 3,
+        ]);
     }
 }
