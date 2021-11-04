97cebf1f2bda52b5f9adee26bfc612ff98defa0d
rendi-18 <rendidwipratama99@gmail.com>
2021-11-04 22:02:39 +0700

    Nambah View

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('content')
+
+@include('layouts.carousel.carousel')
+
+<div class="row">
+    <div class="col-lg-4">
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
